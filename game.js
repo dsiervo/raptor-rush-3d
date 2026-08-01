eaba3fe3961cbdde5b2f8f2317bc3d587d4cd404
Daniel Siervo <emetdan@gmail.com>
2026-07-31 19:24:27 -0500

    Bust cache for generated 3D game modules

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 (async () => {
   try {
-    const version = '20260731-1512';
+    const version = '20260801-3d3';
     const files = ['game-1.js', 'game-2.js', 'game-3.js'];
     const parts = await Promise.all(files.map(async (file) => {
       const response = await fetch(`${file}?v=${version}`, { cache: 'no-store' });
